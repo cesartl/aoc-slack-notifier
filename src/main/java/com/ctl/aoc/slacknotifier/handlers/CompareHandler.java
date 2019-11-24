a0b9ebb9787f359e3710fff10a4969cb9cefe66c
Cesar TL <cesar.tronlozai@gmai.com>
2019-11-24 16:30:32 +0000

    Fixed issue with SQS empty body

--- src/main/java/com/ctl/aoc/slacknotifier/handlers/CompareHandler.java
+++ src/main/java/com/ctl/aoc/slacknotifier/handlers/CompareHandler.java
@@ -1,6 +1,8 @@
 package com.ctl.aoc.slacknotifier.handlers;
 
+import com.amazonaws.services.lambda.runtime.events.SQSEvent;
+import org.springframework.cloud.function.adapter.aws.SpringBootRequestHandler;
 import org.springframework.cloud.function.adapter.aws.SpringBootStreamHandler;
 
-public class CompareHandler extends SpringBootStreamHandler {
+public class CompareHandler extends SpringBootRequestHandler<SQSEvent, Void> {
 }
